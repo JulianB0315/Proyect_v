af671795f2ab2d81170ea5c707fcc32deaafda9a
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-08-10 15:10:50 -0500

    xd

--- Proyect_v/src/main/java/com/mycompany/proyect_v/Proyect_v.java
+++ Proyect_v/src/main/java/com/mycompany/proyect_v/Proyect_v.java
@@ -13,5 +13,7 @@ public class Proyect_v {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
