fc6521efc502bbf6f2f2d779da8a5208660f5387
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-08-10 14:45:40 -0500

    Inicio

---
+++ Proyect_v/src/main/java/com/mycompany/proyect_v/Proyect_v.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyect_v;
+
+/**
+ *
+ * @author Julian
+ */
+public class Proyect_v {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
