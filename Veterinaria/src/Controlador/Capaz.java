436d920e8009b5c46fdc381a5d0ff03f85ba5c57
Julian Burga Bracamonte 🤓 <julianburgabracamonte@gmail.com>
2024-08-23 17:43:41 -0500

    Create Capaz.java

---
+++ Veterinaria/src/Controlador/Capaz.java
@@ -0,0 +1,11 @@
+package Controlador;
+
+import Ventanas.inicio;
+
+public class Capaz {
+    public static void main(String[] args) {
+        inicio pantallaInicio =new inicio();
+        pantallaInicio.setVisible(true);
+        pantallaInicio.setLocationRelativeTo(null);
+    }   
+}
